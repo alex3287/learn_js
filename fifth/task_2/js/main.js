0ed445bd9c2d3a9043458365dd1351a1a04910a7
Alexander Mangazeev <alex3287@bk.ru>
2019-04-27 01:35:18 +0700

    create fifth lesson. Second task, start

---
+++ fifth/task_2/js/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// task 1
+let startCalculating = document.getElementById('start');
+
+// task 2
+let nameValues = document.querySelectorAll('{\w}*5-value');  //getElementsByClassName('-value');
+
+// task 3
+let mandatoryExpense = document.getElementsByClassName('expenses-item');
+
+// task 4
+console.log(nameValues[0]);
+
+
