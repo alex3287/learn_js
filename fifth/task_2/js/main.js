9817f77b713fed116968f5b59e41146447c29584
Alexander Mangazeev <alex3287@bk.ru>
2019-04-27 18:29:42 +0700

    create fifth lesson. task 2

--- fifth/task_2/js/main.js
+++ fifth/task_2/js/main.js
@@ -4,21 +4,38 @@
 let startCalculating = document.getElementById('start');
 
 // task 2
-//let nameValues = document.querySelectorAll('{\w}*5-value');  //getElementsByClassName('-value');
+let nameValues = [];
+nameValues[0] = document.getElementsByClassName('budget-value');
+nameValues[1] = document.getElementsByClassName('daybudget-value');
+nameValues[2] = document.getElementsByClassName('level-value');
+nameValues[3] = document.getElementsByClassName('expenses-value');
+nameValues[4] = document.getElementsByClassName('optionalexpenses-value');
+nameValues[5] = document.getElementsByClassName('income-value');
+nameValues[6] = document.getElementsByClassName('monthsavings-value');
+nameValues[7] = document.getElementsByClassName('yearsavings-value');
 
 // task 3
 let mandatoryExpense = document.getElementsByClassName('expenses-item');
 
 // task 4
-//let approve = document.getElementsByTagName('button'),
-//   calculate;
+let buttons = document.getElementsByTagName('button'),
+  approve_1 = buttons[0],
+  approve_2 = buttons[1],
+  calculate = buttons[2];
 
 // task 5
 let costs = document.querySelectorAll('.optionalexpenses-item');
 
 // task 6
+let income = document.querySelector('.choose-income'),
+  save = document.getElementsByClassName('checksavings'),
+  flag = save[0].querySelector('input'),
+  summa = document.querySelector('.choose-sum'),
+  percent = document.querySelector('.choose-percent'),
+  year = document.querySelector('.year-value'),
+  month = document.querySelector('.month-value'),
+  day = document.querySelector('.day-value');
 
 
-console.log(costs[0]);
 
 
