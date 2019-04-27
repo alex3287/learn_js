0dbd3b13e0ba0cb3fdca26602c3f1e6f4a9e70d6
Alexander Mangazeev <alex3287@bk.ru>
2019-04-27 09:29:39 +0700

    create fifth lesson. Second tasks 1, 3, 5

--- fifth/task_2/js/main.js
+++ fifth/task_2/js/main.js
@@ -4,12 +4,21 @@
 let startCalculating = document.getElementById('start');
 
 // task 2
-let nameValues = document.querySelectorAll('{\w}*5-value');  //getElementsByClassName('-value');
+//let nameValues = document.querySelectorAll('{\w}*5-value');  //getElementsByClassName('-value');
 
 // task 3
 let mandatoryExpense = document.getElementsByClassName('expenses-item');
 
 // task 4
-console.log(nameValues[0]);
+//let approve = document.getElementsByTagName('button'),
+//   calculate;
+
+// task 5
+let costs = document.querySelectorAll('.optionalexpenses-item');
+
+// task 6
+
+
+console.log(costs[0]);
 
 
