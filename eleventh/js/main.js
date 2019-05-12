b7e12d3d5fb64a9595e5febca5010d9fd5ec2a89
Alexander Mangazeev <alex3287@bk.ru>
2019-05-12 22:20:20 +0700

    create lesson eleventh

---
+++ eleventh/js/main.js
@@ -0,0 +1,15 @@
+
+"use strict";
+var a = 9;
+let options = {
+  width: 1366,
+  height: 768,
+  background: 'red',
+  font: {
+    size: '16px',
+    color: '#fff'
+  }
+};
+
+console.log(JSON.parse(JSON.stringify(options)));
+console.log("hello");
